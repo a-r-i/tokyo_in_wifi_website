355568ac24b64c8b57a0a48fe4449cd826017ccd
ari <nemuriobjet@gmail.com>
2018-12-27 05:32:51 +0900

    fix test_views

--- tokyo_in_map/tests/test_views.py
+++ tokyo_in_map/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+
 class TestIndexView(TestCase):
     def test_get(self):
         response = self.client.get('/')
@@ -10,15 +11,18 @@ class TestIndexView(TestCase):
 class TestSpotsView(TestCase):
     fixtures = ['test_views.json']
 
+    def setUp(self):
+        # 新宿駅の緯度と経度
+        self.data = {
+            'latitude': 35.6895924,
+            'longitude': 139.7004131
+        }
+
     def test_get(self):
         response = self.client.get('/spots')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'tokyo_in_map/spots.html')
 
     def test_post(self):
-        data = {
-            'latitude': 35.6895924,
-            'longitude': 139.7004131
-        }
-        response = self.client.post('/spots', data)
+        response = self.client.post('/spots', self.data)
         self.assertEquals(response.status_code, 200)
